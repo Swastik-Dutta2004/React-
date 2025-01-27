8bceea7aa2b711e239e5da79f87a2da3a26184d1
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-01-28 00:50:51 +0530

    learning react from scratch.

--- app/page.js
+++ app/page.js
@@ -1,27 +1,15 @@
-"use client";
 import React from 'react'
 
-function page() {
-  let marks = 80
-  const changeMarks = ()=>{
-    console.log(marks);
-    marks = 33
-    console.log(marks);
-  };
+const page = () => {
+  let marks = 80 
   return (
     <>
-    <h1 className='font-bold text-2xl'>
-      My total mraks were {marks}
-    </h1>
-    <button onClick= {()=> {changeMarks();
-    }}
-    className='bg-gray-400 px-5 py-2 rounded mt-5 text-white font-bold'>
-      update
-    </button>
+    <h1>My marks were {marks}</h1>
+    <button onClick={()=>{
+      marks = 33
+    }}>update</button>
     </>
   )
-    
-  
 }
 
 export default page
